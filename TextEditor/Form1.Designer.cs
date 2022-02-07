e338cf790fe7d9e1d2625da16edd6e05e0120e12
Captain-altf4 <leealex.2006@mail.ru>
2022-02-07 18:03:56 +0700

    added anchor

--- TextEditor/Form1.Designer.cs
+++ TextEditor/Form1.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace TextEditor
 {
-    partial class Form1
+    partial class ms_MainMenu
     {
         /// <summary>
         ///  Required designer variable.
@@ -29,13 +29,48 @@ namespace TextEditor
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.rtb_Editor = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(342, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // rtb_Editor
+            // 
+            this.rtb_Editor.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.rtb_Editor.Location = new System.Drawing.Point(12, 12);
+            this.rtb_Editor.Name = "rtb_Editor";
+            this.rtb_Editor.Size = new System.Drawing.Size(318, 374);
+            this.rtb_Editor.TabIndex = 1;
+            this.rtb_Editor.Text = "";
+            // 
+            // ms_MainMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(342, 398);
+            this.Controls.Add(this.rtb_Editor);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "ms_MainMenu";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.RichTextBox rtb_Editor;
     }
 }
 
--- TextEditor/Form1.cs
+++ TextEditor/Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace TextEditor
 {
-    public partial class Form1 : Form
+    public partial class ms_MainMenu : Form
     {
-        public Form1()
+        public ms_MainMenu()
         {
             InitializeComponent();
         }
